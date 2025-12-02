948953d72f150b5208e7995e27ada1aea489e0e8
shafosho <shabilasofea1@gmail.com>
2025-12-03 00:40:08 +0800

    Docs: Added Javadoc to Score.java

--- src/main/java/com/comp2042/gameLogic/Score.java
+++ src/main/java/com/comp2042/gameLogic/Score.java
@@ -3,18 +3,33 @@ package com.comp2042.gameLogic;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
+/**
+ * Manages the player's score.
+ * Uses JavaFX properties so the GUI can automatically update when the score changes.
+ */
 public final class Score {
 
     private final IntegerProperty score = new SimpleIntegerProperty(0);
 
+    /**
+     * Gets the score property for binding to the GUI.
+     * @return The IntegerProperty containing the current score
+     */
     public IntegerProperty scoreProperty() {
         return score;
     }
 
+    /**
+     * Adds points to the current score.
+     * @param i The amount of points to add
+     */
     public void add(int i){
         score.setValue(score.getValue() + i);
     }
 
+    /**
+     * Resets the score to zero.
+     */
     public void reset() {
         score.setValue(0);
     }
